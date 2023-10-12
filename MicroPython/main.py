1e55a0cbf9f4f4015dceef5a0ebd9eda1287851b
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-10-12 21:26:01 +0000

    python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,34 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Ihor Chernyshev
+Created on: Oct 2023
+This program uses the RobotBit's Neopixels to create Traffic Light
 """
 
 from microbit import *
+import neopixel
 
+display.clear()
+np = neopixel.NeoPixel(pin16, 4)
+np[0] = (0, 0, 0)
+np[1] = (0, 0, 0)
+np[2] = (0, 0, 0)
+np[3] = (255, 255, 255)
+np.clear()
+display.show(Image.HAPPY)
 
-display.scroll("Hello, World!")
+while True:
+    np[2] = (0, 255, 0)
+    print(np[2])
+    np.show()
+    sleep(1000)
+    np.clear()
+    np[1] = (255, 255, 0)
+    print(np[1])
+    np.show()
+    sleep(1000)
+    np.clear()
+    np[0] = (255, 0, 0)
+    print(np[0])
+    np.show()
+    sleep(1000)
+    np.clear()
