8a2581cb4999d73a9c9ab5e38ff6e9565f95aff8
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-10-13 13:26:25 -0400

    typescript version

--- main.ts
+++ main.ts
@@ -16,17 +16,21 @@ neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.show()
 basic.showIcon(IconNames.Happy)
 
+// comment here
 basic.forever(function () {
+  // turning on green
   neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
   neopixelStrip.show()
   basic.pause(1000)
+
+  // turning on yellow
   neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-  neopixelStrip.show()
   neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
   neopixelStrip.show()
   basic.pause(1000)
+
+  // turning on red
   neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-  neopixelStrip.show()
   neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
   neopixelStrip.show()
   basic.pause(1000)
