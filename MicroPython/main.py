4cf152d9be0e9bc25a0b052c3a1c323433f5e9c2
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-10-17 17:11:58 +0000

    python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -4,6 +4,7 @@ Created on: Oct 2023
 This program uses the RobotBit's Neopixels to create Traffic Light
 """
 
+# setup
 from microbit import *
 import neopixel
 
@@ -12,21 +13,26 @@ np = neopixel.NeoPixel(pin16, 4)
 np[0] = (0, 0, 0)
 np[1] = (0, 0, 0)
 np[2] = (0, 0, 0)
-np[3] = (255, 255, 255)
+np[3] = (0, 0, 0)
 np.clear()
 display.show(Image.HAPPY)
 
 while True:
+    # turning on green
     np[2] = (0, 255, 0)
     print(np[2])
     np.show()
     sleep(1000)
     np.clear()
+
+    # turning on yellow
     np[1] = (255, 255, 0)
     print(np[1])
     np.show()
     sleep(1000)
     np.clear()
+
+    # turning on red
     np[0] = (255, 0, 0)
     print(np[0])
     np.show()
